807a6b97667f34f86b1d30e80b2ee81958aca3ec
SergeyYurievich <syurievich@gmail.com>
2016-12-22 16:35:05 +0300

    Deployed f94454e with MkDocs version: 0.16.0

---
+++ assets/javascripts/components/Material/Listener/Viewport/Media.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Martin Donath <martin.donath@squidfunk.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/* ----------------------------------------------------------------------------
+ * Definition
+ * ------------------------------------------------------------------------- */
+
+export default class Media {
+
+  /**
+   * Listener which checks for media queries on dimension changes
+   *
+   * @constructor
+   * @param {string} query - Media query
+   * @param {Function} handler - Event handler to execute
+   */
+  constructor(query, handler) {
+    this.media_   = window.matchMedia(query)
+    this.handler_ = media => {
+      handler(media)
+    }
+  }
+
+  /**
+   * Register listener for media query check
+   */
+  listen() {
+    this.media_.addListener(this.handler_)
+    this.handler_(this.media_)
+  }
+
+  /**
+   * Unregister listener for media query check
+   */
+  unlisten() {
+    this.media_.removeListener(this.handler_)
+  }
+}
